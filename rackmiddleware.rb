ea5ddb6188aa995fc77e8491fd55a20c1d13cae3
Patrick Morris <prmwrites@gmail.com>
2015-07-20 00:28:21 -0400

    SinatraRack App

--- rackmiddleware.rb
+++ rackmiddleware.rb
@@ -6,7 +6,7 @@ class RackMiddleware
     start = Time.now
     status, headers, body = @appl.call(env) # call our Sinatra app
     stop = Time.now
-    puts "Response Time: \#{stop-start}" # display on console
+    puts "Response Time: #{stop-start}" # display on console
     [status, headers, body]
   end
 end
